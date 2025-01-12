6b998b729f1816dac313b8bac0c56909ca144b80
Anttonilv <antton00@outlook.com>
2025-01-12 22:32:03 +0200

    Ensimmäinen versio repositorysta

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-using namespace std;
+int game(int maxnum);
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    int maxnum = 40; // Korkein etsittävä luku
+    std::cout << "Peli alkaa! Arvaa luku valilta 1-" << maxnum << ".\n";
+
+    int arvaustenMaara = game(maxnum);
+    std::cout << "Pelasit pelin loppuun " << arvaustenMaara << " arvauksella.\n";
+
     return 0;
+}
+
+int game(int maxnum) {
+    std::srand(std::time(0));
+    int satunnainenLuku = std::rand() % maxnum + 1;
+    int arvaus = 0;
+    int arvaustenLkm = 0;
+
+    while (arvaus != satunnainenLuku) {
+        std::cout << "Anna arvauksesi: ";
+        std::cin >> arvaus;
+        arvaustenLkm++;
+
+        if (arvaus < satunnainenLuku) {
+            std::cout << "Luku on suurempi.\n";
+        } else if (arvaus > satunnainenLuku) {
+            std::cout << "Luku on pienempi.\n";
+        } else {
+            std::cout << "Oikea vastaus! Luku oli " << satunnainenLuku << ".\n";
+        }
+    }
+
+    return arvaustenLkm;
 }
